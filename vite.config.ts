dc85ca8edc9e3a7cf304834b1d5ca26c0ff60add
Rebelein <rebelein.app@gmail.com>
2025-12-04 02:53:54 +0100

    Update Service Worker version to 0.0.1 in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,8 @@ export default defineConfig(({ mode }) => {
     ],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.APP_VERSION': JSON.stringify("0.0.1"), // Sync with package.json
     },
     resolve: {
       alias: {
